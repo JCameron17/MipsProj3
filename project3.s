55c6305ab936cb22d8f2956c79fc42d77bf185e4
JCameron17 <jasmine.cameron@bison.howard.edu>
2019-11-26 23:58:44 -0500

    get input ready to split substrings and calculate decimal

--- project3.s
+++ project3.s
@@ -8,3 +8,8 @@
     la $a0, myString  #ask for user input
     li $a1, 1001      #allocate space for input
     syscall
+
+    #get input ready to split substrings and calculate decimal 
+    la $s2, myString  #move string to $s2 register
+    li $s0, 0
+    li $s1, 0
