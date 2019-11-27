798c97d29a1dc77ff254086c30f0dfc6a2211c22
JCameron17 <jasmine.cameron@bison.howard.edu>
2019-11-27 00:15:20 -0500

    load message for invalid input

--- project3.s
+++ project3.s
@@ -9,7 +9,11 @@
     li $a1, 1001      #allocate space for input
     syscall
 
-    #get input ready to split substrings and calculate decimal 
+    #get input ready to split substrings and calculate decimal
     la $s2, myString  #move string to $s2 register
     li $s0, 0
     li $s1, 0
+
+    invalidMessage:
+      li $v0, 4
+      la $t0, invalid   #load message to print for invalid input
